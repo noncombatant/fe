94fc87d4d2c84c3b9e89539812f8da914bb7478d
Chris Palmer <chris@noncombatant.org>
2024-08-04 12:31:33 -0700

    Fancier, more reassuring test output

--- test.sh
+++ test.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+failed=0
 for s in scripts/*; do
     b=$(basename "$s")
     ./fe "$s" > out 2> err
-    if ! cmp out "tests/$b.out" || ! cmp err "tests/$b.err"; then
-        echo "$s failed" > /dev/stderr
-        exit 1
+    if cmp out "tests/$b.out" && cmp err "tests/$b.err"; then
+        echo "✅ $s"
+    else
+        echo "❌ $s"
+        failed=$(($failed + 1))
     fi
 done
 
 rm out err
+
+if [[ $failed -eq 0 ]]; then
+    echo "✅ all tests passed"
+    exit 0
+else
+    echo "❌ $failed tests failed"
+    exit 1
+fi
