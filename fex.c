bc053d16bdc3af17fe4249bec9b09f3a136586bb
Chris Palmer <chris@noncombatant.org>
2024-08-04 17:13:35 -0700

    Add the `fpclassify` functions to Fex

--- fex.c
+++ fex.c
@@ -11,6 +11,10 @@ void FexInstallNativeFn(FeContext* ctx, const char* name, FeNativeFn fn) {
 
 void FexInstallNativeFns(FeContext* ctx) {
   FexInstallNativeFn(ctx, "abs", FexAbs);
+  FexInstallNativeFn(ctx, "is-finite", FexIsFinite);
+  FexInstallNativeFn(ctx, "is-infinite", FexIsInfinite);
+  FexInstallNativeFn(ctx, "is-nan", FexIsNaN);
+  FexInstallNativeFn(ctx, "is-normal", FexIsNormal);
   FexInstallNativeFn(ctx, "max", FexMax);
   FexInstallNativeFn(ctx, "min", FexMin);
   FexInstallNativeFn(ctx, "pow", FexPow);
@@ -19,6 +23,26 @@ void FexInstallNativeFns(FeContext* ctx) {
 FeObject* FexAbs(FeContext* ctx, FeObject* arg) {
   double x = FeToNumber(ctx, FeGetNextArgument(ctx, &arg));
   return FeMakeNumber(ctx, fabs(x));
+}
+
+FeObject* FexIsFinite(FeContext* ctx, FeObject* arg) {
+  double x = FeToNumber(ctx, FeGetNextArgument(ctx, &arg));
+  return FeMakeBool(ctx, isfinite(x));
+}
+
+FeObject* FexIsInfinite(FeContext* ctx, FeObject* arg) {
+  double x = FeToNumber(ctx, FeGetNextArgument(ctx, &arg));
+  return FeMakeBool(ctx, isinf(x));
+}
+
+FeObject* FexIsNaN(FeContext* ctx, FeObject* arg) {
+  double x = FeToNumber(ctx, FeGetNextArgument(ctx, &arg));
+  return FeMakeBool(ctx, isnan(x));
+}
+
+FeObject* FexIsNormal(FeContext* ctx, FeObject* arg) {
+  double x = FeToNumber(ctx, FeGetNextArgument(ctx, &arg));
+  return FeMakeBool(ctx, isnormal(x));
 }
 
 FeObject* FexMax(FeContext* ctx, FeObject* arg) {
--- fex.h
+++ fex.h
@@ -7,6 +7,10 @@ void FexInstallNativeFn(FeContext* ctx, const char* name, FeNativeFn fn);
 void FexInstallNativeFns(FeContext* ctx);
 
 FeObject* FexAbs(FeContext* ctx, FeObject* arg);
+FeObject* FexIsFinite(FeContext* ctx, FeObject* arg);
+FeObject* FexIsInfinite(FeContext* ctx, FeObject* arg);
+FeObject* FexIsNaN(FeContext* ctx, FeObject* arg);
+FeObject* FexIsNormal(FeContext* ctx, FeObject* arg);
 FeObject* FexMax(FeContext* ctx, FeObject* arg);
 FeObject* FexMin(FeContext* ctx, FeObject* arg);
 FeObject* FexPow(FeContext* ctx, FeObject* arg);
