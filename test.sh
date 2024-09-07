cf1b3585c679b5a0a5d66412d98cb4cb4fd52c2d
Chris Palmer <chris@noncombatant.org>
2024-09-06 20:16:05 -0700

    Shellcheck and shfmt

--- test.sh
+++ test.sh
@@ -5,26 +5,27 @@ set -euo pipefail
 failed=0
 
 check_results() {
-    local expected_out=${1}
-    local expected_err=${2}
-    local comment=${3}
-    if cmp out "$expected_out" && cmp err "$expected_err"; then
-        echo "✅ $comment"
-    else
-        echo "❌ $comment"
-        failed=$(($failed + 1))
-    fi
+  local expected_out=${1}
+  local expected_err=${2}
+  local comment=${3}
+  if cmp out "$expected_out" && cmp err "$expected_err"; then
+    echo "✅ $comment"
+  else
+    echo "❌ $comment"
+    failed=$((failed + 1))
+  fi
 }
 
 run_test() {
-    for s in scripts/*; do
-        local b=$(basename "$s")
-        ./fe scripts/assert.fe "$s" > out 2> err
-        check_results "tests/$b.out" "tests/$b.err" "$s"
-    done
-    ./fe -e '(print "hello, world!")' > out 2> err
-    check_results "tests/one-liner.out" "tests/one-liner.err" "one-liner"
-    rm out err
+  for s in scripts/*; do
+    local b
+    b=$(basename "$s")
+    ./fe scripts/assert.fe "$s" > out 2> err
+    check_results "tests/$b.out" "tests/$b.err" "$s"
+  done
+  ./fe -e '(print "hello, world!")' > out 2> err
+  check_results "tests/one-liner.out" "tests/one-liner.err" "one-liner"
+  rm out err
 }
 
 make clean
@@ -35,8 +36,8 @@ RELEASE=1 make fe
 run_test
 
 if [[ $failed -eq 0 ]]; then
-    echo "✅ all tests passed"
+  echo "✅ all tests passed"
 else
-    echo "❌ $failed tests failed"
+  echo "❌ $failed tests failed"
 fi
 exit "$failed"
